6307c1e1b12ff5ba6f9b6a601e08296cba5ea5f5
muhammedsuhair74 <muhammedsuhair74@gmail.com>
2020-04-14 22:01:48 +0530

    background generator

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var a=document.getElementsByTagName("H1");
-//a.text()="chnaged";
+var css=document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var back=document.getElementById("gradient");
+var text3=document.querySelector(".abc");
 
-const x = document.getElementsByClassName("abc");
-x.addEventListener("click", function() {
-	a.innerHTML="chnaged";	
-});
+function setgradient()
+{
+	back.style.background="linear-gradient(to right," + color1.value + ", " +color2.value + " )";	
+	css.textContent=back.style.background+";";
+}
+color1.addEventListener("input",setgradient);
+color2.addEventListener("input",setgradient);
+text3.style.backgroundColor="green";
