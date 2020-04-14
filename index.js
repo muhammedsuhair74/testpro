ce84fa666e5cfbc3a9d668a0599bc4d5fc60bdc4
muhammedsuhair74 <muhammedsuhair74@gmail.com>
2020-04-14 13:10:39 +0530

    newl

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-class player
-{
-	constructor(name,age)
-	{
-		this.name=name;
-		this.age=age;
-	}
-	introduce()
-	{
-		console.log('hi i am '+ this.name);
-	}
-}
-class newplayer extends player{
-	constructor(name,age)
-	{
-		super(name,age)
-	
-	}
-	introduce()
-	{
-		console.log("hi my name is "+ this.name+" and of age "+this.age);
-	}
-}
-const a=new player("suahir",23);
-const b=new newplayer();
-.introduce();
+var a=document.getElementsByTagName("H1");
+//a.text()="chnaged";
+
+const x = document.getElementsByClassName("abc");
+x.addEventListener("click", function() {
+	a.innerHTML="chnaged";	
+});
